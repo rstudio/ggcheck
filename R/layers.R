9f50ed6df293777499c32a78da6691e31123954c
Garrett Grolemund <grolemund@gmail.com>
2020-08-21 11:33:52 -0400

    Documents layer functions

--- R/layers.R
+++ R/layers.R
@@ -1,8 +1,52 @@
 
+#' How many layers are in a plot?
+#'
+#' @param p A ggplot object
+#'
+#' @return Numeric. The number of layers.
+#' @export
+#'
+#' @examples
+#' p <- ggplot(data = mpg, mapping = aes(x = displ, y = hwy)) +
+#'   geom_point(mapping = aes(color = class)) +
+#'   geom_smooth()
+#' n_layers(p)
 n_layers <- function(p) {
   length(p$layers)
 }
 
+#'Isolate a layer from a plot
+#'
+#'\code{get_layer} returns a layer from a plot along with the global data sets
+#'and aesthetic mappings that the layer may inherit from.
+#'
+#'Users can specify a layer in one of three ways:
+#'
+#'\enumerate{
+#'  \item By order of appearance with \code{i}. The first layer to appear in the
+#'  plot (the one drawn first, on the bottom) corresponds to \code{i = 1}.
+#'  \item By type of geom with \code{geom}. \code{get_layer} will return the
+#'  first layer that uses the geom.
+#'  \item By a combination of \code{geom} and
+#'  \code{i}. \code{get_layer} will return the ith layer that uses the geom.
+#'}
+#'
+#'@param p A ggplot object
+#'@param geom A character string found in the suffix of a ggplot2 geom function,
+#'  e.g. \code{"point"}.
+#'@param i A numerical index, e.g. \code{1}.
+#'
+#'@return An object with class \code{layer_to_check} to be manipulated further
+#'  with ggcheck functions.
+#'@export
+#'
+#' @examples
+#' p <- ggplot(data = mpg, mapping = aes(x = displ, y = hwy)) +
+#'   geom_point(mapping = aes(color = class)) +
+#'   geom_smooth()
+#'
+#' get_layer(p, i = 1)
+#' get_layer(p, geom = "smooth")
 get_layer <- function(p, geom = NULL, i = NULL) {
 
   if(!inherits(p, "ggplot")) {
