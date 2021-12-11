958a936f6678ab8010f8a9e8c4b11bd6d9a465c8
Alex Rossell Hayes <alexander@rossellhayes.com>
2021-12-10 17:28:49 -0800

    Add tests for `stop_if_not_ggplot()`

--- tests/testthat/test-is_ggplot.R
+++ tests/testthat/test-is_ggplot.R
@@ -1,14 +1,24 @@
 require(ggplot2)
 
+p_valid <- ggplot(data = mpg, mapping = aes(x = displ, y = hwy)) +
+  geom_point()
+
+p_invalid <- geom_point()
+
+p_basic <- ggplot(data = mpg, mapping = aes(x = displ, y = hwy))
+
+p_minimal <- ggplot()
 
 test_that("is_ggplot", {
-  p_valid <- ggplot(data = mpg, mapping = aes(x = displ, y = hwy)) +
-    geom_point()
   expect_true(is_ggplot(p_valid))
-
-  p_invalid <- geom_point()
   expect_false(is_ggplot(p_invalid))
-
-  p_basic <- ggplot(data = mpg, mapping = aes(x = displ, y = hwy))
   expect_true(is_ggplot(p_basic))
+  expect_true(is_ggplot(p_minimal))
+})
+
+test_that("stop_if_not_ggplot", {
+  expect_invisible(expect_null(stop_if_not_ggplot(p_valid)))
+  expect_error(stop_if_not_ggplot(p_invalid))
+  expect_invisible(expect_null(stop_if_not_ggplot(p_basic)))
+  expect_invisible(expect_null(stop_if_not_ggplot(p_minimal)))
 })
